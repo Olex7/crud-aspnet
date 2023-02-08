ef94aa8eb182b497617b54054b22daa03d9702a0
Olex7 <alexandrurus8@gmail.com>
2023-02-08 17:56:20 +0200

    Revert "Revert "Add project files.""
    This reverts commit b7a6fae5fd8547d7216854d7600e56396b0713a4.

---
+++ crud aspnet/Data/MVCDEMODbContext.cs
@@ -0,0 +1,17 @@
+﻿using crud_aspnet.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_aspnet.Data
+{
+    public class MVCDEMODbContext : DbContext
+    {
+        public MVCDEMODbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+    }
+}
