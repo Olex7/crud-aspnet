b7a6fae5fd8547d7216854d7600e56396b0713a4
Olex7 <alexandrurus8@gmail.com>
2023-02-08 17:52:26 +0200

    Revert "Add project files."
    This reverts commit a2ddd1c3705dd28fa6d309a086923467f9572969.

--- crud aspnet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace crud_aspnet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
